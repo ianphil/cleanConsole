5bc384c94cabe9239e6dbad0d0b03aac2d3d6b7f
Ian Philpot <ian.philpot@microsoft.com>
2023-02-05 10:54:38 -0500

    Adds InfraExtensions fix and devContainer for my linux machine

--- src/CleanConsole/CleanConsole.Infrastructure/Utilities/InfrastructureExtensions.cs
+++ src/CleanConsole/CleanConsole.Infrastructure/Utilities/InfrastructureExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using CleanConsole.Infrastructure.Configuration;
+using CleanConsole.Infrastructure.Services;
+using CleanConsole.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,5 +14,22 @@ public static class InfrastructureExtensions
     {
         var dbOptions = configuration.GetRequiredSection("Database").Get<WeatherDbOptions>();
         dbOptions.Validate();
+
+        services.AddDbContext<SimplifiedContext>(opt =>
+        {
+            var connectionString = $"Server={dbOptions.Server};Database={dbOptions.Database};Username={dbOptions.Username};Password={dbOptions.Password}";
+            opt.UseSqlServer(connectionString);
+        });
+
+        var weatherApiOptions = configuration.GetRequiredSection("WeatherApi").Get<WeatherApiOptions>();
+        weatherApiOptions.Validate();
+
+        services.AddHttpClient<IWeatherApiClient, WeatherApiClient>(nameof(IWeatherApiClient), client =>
+        {
+            client.BaseAddress = new Uri(weatherApiOptions.BaseUrl);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", weatherApiOptions.Token);
+        });
+
+        return services;
     }
 }
