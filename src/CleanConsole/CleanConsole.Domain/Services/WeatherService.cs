09fe97c97aec195088f1ad000d10faf6b75e0736
Ian Philpot <ian.philpot@microsoft.com>
2023-02-05 12:00:41 -0500

    Updates domain, time for UI

--- src/CleanConsole/CleanConsole.Domain/Services/WeatherService.cs
+++ src/CleanConsole/CleanConsole.Domain/Services/WeatherService.cs
@@ -1,5 +1,8 @@
 ﻿using CleanConsole.Models;
 using CleanConsole.Models.Output;
+using CleanConsole.Entities;
+using CleanConsole.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanConsole.Domain.Services;
 
@@ -8,10 +11,62 @@ public interface IWeatherService
     Task<OperationResult<WeatherDto>> GetWeatherDataAsync(Guid currentAccountId, double latitude, double logitude);
 }
 
-public class WeatherService : IWeatherService
+internal class WeatherService : IWeatherService
 {
-    public Task<OperationResult<WeatherDto>> GetWeatherDataAsync(Guid currentAccountId, double latitude, double logitude)
+    private readonly SimplifiedContext _dbContext;
+    private readonly IWeatherApiClient _weatherApiClient;
+
+    public WeatherService(SimplifiedContext dbContext, IWeatherApiClient weatherApiClient)
     {
-        throw new NotImplementedException();
+        _dbContext = dbContext;
+        _weatherApiClient = weatherApiClient;
+    }
+
+    public async Task<OperationResult<WeatherDto>> GetWeatherDataAsync(Guid currentAccountId, double latitude, double longitude)
+    {
+        if (!await _dbContext.Accounts.AnyAsync(x => x.Id == currentAccountId && x.Enabled))
+        {
+            return OperationResultStatus.Forbidden;
+        }
+
+        var existingData = await _dbContext.WeatherForecasts
+            .SingleOrDefaultAsync(x => x.Id == currentAccountId && x.Latitude == latitude && x.Longitude == longitude);
+
+        if (existingData != null)
+        {
+            return new WeatherDto
+            {
+                Latitude = latitude,
+                Longitude = longitude,
+                Temerature = existingData.Temperature,
+                Unit = existingData.Unit
+            };
+        }
+
+        var weatherAnalyticsData = await _weatherApiClient.GetWeatherDataAsync(latitude, longitude);
+
+        if (weatherAnalyticsData == null)
+            return new OperationResult<WeatherDto>(OperationResultStatus.NotFound, $"Weather data not found for the latitude {latitude} and longitude {longitude}");
+
+        var weatherForecast = new WeatherForecast
+        {
+            AccountId = currentAccountId,
+            Latitude = latitude,
+            Longitude = longitude,
+            Temperature = weatherAnalyticsData.Temperature,
+            Unit = weatherAnalyticsData.Unit,
+            Time = DateTimeOffset.UtcNow
+        };
+
+        _dbContext.WeatherForecasts.Add(weatherForecast);
+        await _dbContext.SaveChangesAsync();
+
+        return new WeatherDto
+        {
+            Latitude = latitude,
+            Longitude = longitude,
+            Temerature = weatherAnalyticsData.Temperature,
+            Unit = weatherAnalyticsData.Unit
+        };
     }
 }
